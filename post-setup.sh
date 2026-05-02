079b7c076fd6b3f5cce3df2d95a19d9320476288
Ваше Имя <vladimirpetrenko1401@gmail.com>
2026-05-03 01:54:39 +0200

    first

---
+++ post-setup.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+set -e
+
+USER=$1
+HOST=$2
+ROOT=$3
+
+echo "==> Setup time and locale..."
+ln -sf /usr/share/zoneinfo/Europe/Warsaw /etc/localtime
+hwclock --systohc
+
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+echo "ru_RU.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen
+
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+
+echo "$HOST" >> /etc/hostname
+
+echo "==> Creating root password..."
+passwd
+
+echo "==> Creating user..."
+useradd -m -G wheel -s /bin/bash "$USER"
+
+echo "Enter the password for $USER:"
+passwd "$USER"
+
+echo "Now You must manually give permissions for $USER"
+echo 'Just uncomment this line ==> "%wheel ALL=(ALL:ALL) ALL"'
+read -p "press enter to continue..."
+EDITOR=nvim visudo
+
+echo "==> Enabling NetworkManager..."
+systemctl enable NetworkManager
+
+echo "==> installing systemd-boot..."
+bootctl install
+
+# Получаем UUID для конфига загрузчика
+UUID=$(blkid -s UUID -o value "$ROOT")
+
+cat <<EOF > /boot/loader/loader.conf
+default  arch.conf
+timeout  0
+console-mode max
+EOF
+
+cat <<EOF > /boot/loader/entries/arch.conf
+title   Arch Linux
+linux   /vmlinuz-linux
+initrd  /initramfs-linux.img
+options root=UUID=$UUID rw
+EOF
+
+echo "==> loading dotfiles..."
+su - "$USER" -c "git clone https://github.com/Iem0n/arch_sync.git ~/my-dotfiles"
+su - "$USER" -c "cd ~/my-dotfiles && ./install.sh"
+
+echo "==> installing greetd and tuigreet..."
+pacman -S --noconfirm greetd greetd-tuigreet
+
+echo "==> Configurating greetd..."
+[ -f /etc/greetd/config.toml ] && mv /etc/greetd/config.toml /etc/greetd/config.toml.bak
+
+cat <<EOF > /etc/greetd/config.toml
+[terminal]
+vt = 1
+
+[default_session]
+command = "tuigreet --time --remember --cmd 'dbus-run-session /home/vova/.local/bin/niri-session'"
+user = "greeter"
+EOF
+
+echo "==> Enabling greetd service..."
+systemctl enable greetd.service
+
+echo "==> Generating ZRAM..."
+pacman -S zram-generator
+cat <<EOF > /etc/systemd/zram-generator.conf
+[zram0]
+zram-size = ram / 2
+compression-algorithm = zstd
+EOF
+
+echo "==> Завершение настройки в chroot."
+
+exit
---
+++ setup.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+set -e
+
+echo "==> List of avalible drives:"
+lsblk -dno NAME,SIZE,MODEL | grep -v "loop"
+read -p "Enter drive name (nvme0n1 or sda): " DISK_NAME
+DISK="/dev/$DISK_NAME"
+
+if [ ! -b "$DISK" ]; then
+    echo "ERROR: Disk $DISK not found!"
+    exit 1
+fi
+
+[[ $DISK =~ "nvme" ]] && P="p" || P=""
+
+echo "Select install mode:"
+echo "1) Dualboot"
+echo "2) Only Linux"
+read -p "Your choice (1/2): " MODE
+
+if [[ $MODE == "1" ]]; then
+    EFI_PART="${DISK}${P}1"
+    ROOT_PART="${DISK}${P}4"
+    FORMAT_EFI=false
+    echo "MODE: Dualboot. EFI ($EFI_PART) will not be formated."
+elif [[ $MODE == "2" ]]; then
+    EFI_PART="${DISK}${P}1"
+    ROOT_PART="${DISK}${P}2"
+    FORMAT_EFI=true
+    echo "MODE: Only Linux. EFI ($EFI_PART) WILL BE FORMATED!"
+else
+    echo "Invalid choice!"
+    exit 1
+fi
+
+echo "WARNING: ROOT ($ROOT_PART) WILL BE FORMATED!"
+read -p "Continue? (y/n): " confirm
+[[ $confirm != "y" ]] && exit 1
+
+echo "==>  Formating root..."
+mkfs.ext4 "$ROOT_PART"
+
+if [ "$FORMAT_EFI" = true ]; then
+    echo "==> Formating EFI..."
+    mkfs.fat -F 32 "$EFI_PART"
+fi
+
+echo "==> Mounting parts..."
+mount "$ROOT_PART" /mnt
+mkdir -p /mnt/boot
+mount "$EFI_PART" /mnt/boot
+
+echo "==> Installing base system..."
+pacstrap -K /mnt base linux linux-firmware linux-headers amd-ucode base-devel networkmanager neovim efibootmgr git
+
+echo "==> Generating fstab..."
+genfstab -U /mnt >> /mnt/etc/fstab
+
+echo "==> Copying post-setup sript..."
+cp post-setup.sh /mnt/post-setup.sh
+chmod +x /mnt/post-setup.sh
+
+echo "==> Enter in chroot..."
+arch-chroot /mnt ./post-setup.sh "vova" "vova-pc" "$ROOT_PART"
+
+read -p "system instaled! press ENTER for reboot"
+
+umount -a
+reboot
